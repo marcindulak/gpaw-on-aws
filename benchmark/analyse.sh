a3d8ff4fe915d62495436d625cb71dd86db1c9b0
Marcin Dulak <Marcin.Dulak@gmail.com>
2015-09-28 11:36:01 +0200

    cpu type

--- benchmark/analyse.sh
+++ benchmark/analyse.sh
@@ -1,4 +1,4 @@
-echo '# Niflheim xeon16'
+echo '# Niflheim xeon16: Intel(R) Xeon(R) CPU E5-2670 0 @ 2.60GHz '
 python scaling.py -v --dir=xeon16 --pattern="xeon16_*_" b256H2O
-echo '# AWS m4.xlarge'
+echo '# AWS m4.xlarge: Intel(R) Xeon(R) CPU E5-2676 v3 @ 2.40GHz'
 python scaling.py -v --dir=m4.xlarge --pattern="m4.xlarge_*_" b256H2O
